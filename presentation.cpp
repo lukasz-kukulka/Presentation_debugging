581162286e42252dc7a545d04d551f7d658a505f
lukasz-kukulka <qqla83@gmail.com>
2021-09-07 14:41:25 +0100

    Complete

--- presentation.cpp
+++ presentation.cpp
@@ -1,15 +1,17 @@
 #include "Jail.hpp"
 #include <utility>
+#include <iostream>
 
-void printJail() {
-
+void printJail(Jail& jail) {
+    std::cout << jail.getName() << " JAIL:\n";
 }
 
 int main() {
     std::pair<int, int>coordination{ 20, 30 };
     Jail jailOne {coordination.first, coordination.second, "Alcatraz"};
+    printJail(jailOne);
     jailOne.openJail();
-    Jail jailTwo {coordination.first, coordination.second, "Alcatraz"};
+    Jail jailTwo {coordination.first, coordination.second, "Guantanamo"};
     jailTwo.openJail();
     return 0;
 }
